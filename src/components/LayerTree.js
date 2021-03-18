6f53a2fe4f7ffe59960c0b038d0a65b37d0a6424
nilaynishant <nilaynishant@gmail.com>
2021-03-18 09:05:01 +0530

    layercontrolccordian

--- src/components/LayerTree.js
+++ src/components/LayerTree.js
@@ -1,10 +1,11 @@
 import Layer from './Layer'
-
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 const LayerTree = ({ tasks, changeLayer, category }) => {
 
   return (
     <div className="LayerTree">
-      <p>{category}</p>
+      <p>{category}<KeyboardArrowDownIcon/></p>
+      
       {tasks.filter((task) => task.class === category).map((task, index) => (
         <Layer key={index} task={task} changeLayer={changeLayer}  />
       ))}
