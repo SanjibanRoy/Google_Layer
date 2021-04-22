dde07f88122d3da40745af5e1805b83bc2dd1cc4
Sanjiban Roy <sanjibanroy053@gmail.com>
2021-04-22 17:38:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,69 +3,102 @@ import Map from './components/Map'
 import InfoBox from './components/InfoBox'
 import Legend from './components/Legend'
 import SideBarWrapper from './components/SideBarWrapper'
-import {layer} from './config'
-import {analytics} from './config'
+import { layer } from './config'
+import { analytics } from './config'
+import { panelVisibilty } from './config'
+import { maps } from './config'
 
 const App = () => {
   const [tasks, setTasks] = useState(layer)
   const [info, setinfo] = useState([])
-  const [analytic,setAnalytics]=useState(analytics)
+  const [analytic, setAnalytics] = useState(analytics)
+  const [visibility, setVisibility] = useState(panelVisibilty)
+  const [mapsvisibility, setmaps] = useState(maps)
+
 
 
   //Update Info box
   const updateInfo = (id) => {
     setinfo(
       [
-      tasks.filter((task)=>task.show===true).map((t)=>
-      {
-        return t.layer
-      })
-      ]      
+        tasks.filter((task) => task.show === true).map((t) => {
+          return t.layer
+        })
+      ]
     )
   }
-
-  // Add Remove Layer
-  const activateAnalytics =  (id) => {
+  // add or remove map 
+  const ChangeMap = (id) => {
     console.log(id)
-    setAnalytics(
-      analytic.map((task) =>
-      task.id == id ? { ...task, show: !task.show } :  { ...task, show: false }
+    setmaps(
+      mapsvisibility.map((task) =>
+        task.id === id ? { ...task, show: !task.show } : { ...task, show: false }
       )
     )
   }
-  const activateLayer =  (id) => {
+
+
+
+
+  // Add Remove Layer
+  const activateAnalytics = (id) => {
+    console.log(id)
+    setAnalytics(
+      analytic.map((task) =>
+        task.id === id ? { ...task, show: !task.show } : { ...task, show: false }
+      )
+    )
+  }
+  const activateLayer = (id) => {
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, show: !task.show } : task
       )
     )
   }
+  //toggle visibility 
+  const changeVisibility = (id) => {
+
+    setVisibility(
+      visibility.map((task) =>
+        task.id === id ? { ...task, show: true } : { ...task, show: false }
+      )
+    )
+    //changeVisibility: false 
 
 
+  }
   return (
-      <div className='container'>
-        <SideBarWrapper
-                tasks={layer}
-                activateLayer={activateLayer}
-                analyticsLayers = {analytics}
-                activateLayer1={activateAnalytics}
-        />
+    <div className='container'>
 
-       <Map 
-        tasks = {tasks}
+      <SideBarWrapper
+        visibility={visibility}
+        tasks={layer}
+        activateLayer={activateLayer}
+        analyticsLayers={analytics}
+        activateLayer1={activateAnalytics}
+        changeVisibility={changeVisibility}
+        ChangeMap={ChangeMap}
+
+
+      />
+
+      <Map
+        tasks={tasks}
         changeLayer={activateLayer}
-        analyticsLayers = {analytic}
-        updateBox = {updateInfo}
-        />
-        <InfoBox
-        info = {info}
-        />
+        analyticsLayers={analytic}
+        updateBox={updateInfo}
+        mapslayer={mapsvisibility}
+      />
+      <InfoBox
+        info={info}
+      />
 
-        <Legend
-        tasks = {tasks}
-        />
-        
-      </div>
+      <Legend
+        tasks={tasks}
+      />
+
+    </div>
   )
 }
 
--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -2,31 +2,48 @@ import React from 'react'
 import CloudIcon from '@material-ui/icons/Cloud';
 import LayersIcon from '@material-ui/icons/Layers';
 import EcoIcon from '@material-ui/icons/Eco';
-
-const SidePanel = ({changeVisibility}) => {
+const SidePanel = ({ changeVisibility }) => {
 
     return (
         <div className="SidePanel">
             <ul>
-                <li className="logo"><img src="./NeSDRLogo1.png"/>
+                <li key={1} ><img src="./NeSDRLogo1.png" alt='nesdr-logo' />
                 </li>
-                <li><CloudIcon/>
-                    <span>Cloud</span>
+                <hr>
+                </hr>
+                <li key={2} onClick={() => changeVisibility('Layer')} >< LayersIcon />
+                    <span>Layer</span>
                 </li>
-                <hr></hr>
-                <li><LayersIcon onClick={() => changeVisibility()}/>
-                <span>Layer</span>
+                <hr>
+                </hr>
+                <li key={3} onClick={() => changeVisibility('Vegetation')} >< EcoIcon />
+                    <span>Vegetation</span>
                 </li>
-                <hr></hr>
-                <li><EcoIcon/>
-                <span>Vegetation</span>
+                <hr>
+                </hr>
+                <li key={4} onClick={() => changeVisibility('Weather')} ><EcoIcon />
+                    <span>Weather</span>
                 </li>
-                <li><CloudIcon/>
-                <span>Cloud</span>
+                <hr>
+                </hr>
+                <li key={5} onClick={() => changeVisibility('Water_Resources')} ><EcoIcon />
+                    <span>Water Resource</span>
                 </li>
+                <hr>
+                </hr>
+                <li key={6} onClick={() => changeVisibility('Satellite_Imagery')} ><CloudIcon />
+                    <span>Satellite Imagery</span>
+                </li>
+                <hr>
+                </hr>
+                <li key={7} onClick={() => changeVisibility('Vedas_Services')} ><CloudIcon />
+                    <span>Vedas Services</span>
+                </li>
+                <hr>
+                </hr>
             </ul>
         </div>
-        
+
     )
 }
 
