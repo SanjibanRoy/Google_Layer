97dda4b1ebc593b36b2da7e22166f02993d93d47
Sanjiban Roy <sanjibanroy053@gmail.com>
2021-04-24 13:50:28 +0530

    24-04-2021

--- src/components/SideBarWrapper.js
+++ src/components/SideBarWrapper.js
@@ -1,29 +1,95 @@
-import React, { useState } from 'react'
+//import necessary lib
+import React, { Component } from 'react'
 import SidePanel from './SidePanel'
 import LayerTree from './LayerTree'
 import AnalyticsLayers from './AnalyticsLayers'
 import SatelliteIcon from '@material-ui/icons/Satellite';
 import MapIcon from '@material-ui/icons/Map';
-// import { panelVisibilty } from '../config'
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
-const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1, changeVisibility, visibility, ChangeMap }) => {
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Select from 'react-select';
+import axios from 'axios'
 
-    // const [visibility, setVisibility] = useState(panelVisibilty)
-    // alert(JSON.stringify(visibility))
-    // var active = Object.keys(panelVisibilty).filter(k => panelVisibilty[k].show);
-    // alert(active)
-    //className={this.props.shouldHide ? 'hidden' : ''}
-    //alert(JSON.stringify(visibility))
+// import CreatableSelect from 'react-select/creatable';
+//end of import 
+
+
+const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1, changeVisibility, visibility, ChangeMap, ndvidates }) => {
+    //Tab view start 
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+
+    };
+    //tab view ends
+
+    //accordion start
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            width: '100%',
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            fontWeight: theme.typography.fontWeightRegular,
+        },
+    }));
+    const classes = useStyles();
+    // accordion ends
+
+    //hind show sideBarwarapper start
     var data_filter = visibility.filter(element => element.show)
     var type = data_filter[0].id
+    // hide show sideBarWrapper ends
+
+
+    async getOptions(){
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+        const data = res.data
+
+        const options = data.map(d => ({
+            "value": d.id,
+            "label": d.name
+
+        }))
+
+
+
+    }
+    // const options = data.map(d => ({
+    //     "value": d.id,
+    //     "label": d.name
+    // }))
+    // console.log(options)
+
+    // handle select for modis
+    // handleChange = (newValue, actionMeta) => {
+    //     console.group('Value Changed');
+    //     console.log(newValue);
+    //     console.log(`action: ${actionMeta.action}`);
+    //     console.groupEnd();
+    // };
+
+    //return view
     return (
         <div className="SideBarWrapper">
+            {/* load of side panel start */}
             <SidePanel
                 changeVisibility={changeVisibility}
             />
-            <div className={type === 'Layer' ? 'LayerContainer' : 'hidden'}>
-                <ul>
+            {/* load of side panel start */}
 
+            {/* *************Layer Tab start*************** */}
+            <div className={type === 'Layer' ? 'LayerContainer' : 'hidden'}>
+                {/* List of map layers */}
+                <ul>
                     <li key={2} onClick={() => ChangeMap('Cartodb')} >< MapIcon />
                         <span> Cartodb</span>
                     </li>
@@ -55,6 +121,7 @@ const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1,
                     <hr>
                     </hr>
                 </ul>
+
                 <LayerTree
                     tasks={tasks}
                     changeLayer={activateLayer}
@@ -72,21 +139,162 @@ const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1,
                 />
 
             </div>
+            {/* *************Layer Tab end*************** */}
 
+
+            {/* *************Vegetation Tab start*************** */}
             <div className={type === 'Vegetation' ? 'LayerContainer' : 'hidden'}>
-                <LayerTree
-                    tasks={tasks}
-                    changeLayer={activateLayer}
-                    category="Admin"
-                    state='test'
-                />
-                <LayerTree
-                    tasks={tasks}
-                    changeLayer={activateLayer}
-                    category="Natural Resource"
-                />
-            </div>
+                {/* Tab View */}
+                <Paper square>
+                    <Tabs
+                        value={value}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        onChange={handleChange}
+                        aria-label="disabled tabs example"
+                    >
+                        <Tab label="Visualization" />
+                        <Tab label="Analysis" />
 
+                    </Tabs>
+
+                </Paper>
+                {/* Tab1 Data */}
+                <div className={value === 0 ? ' ' : 'hidden'}>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography className={classes.heading}>MODIS NDVI</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+
+                            <Select className="css-e56m7-control"
+                                defaultValue={ndvidates[2]}
+                                label="Single select"
+                                options={ndvidates}
+                                theme={theme => ({
+                                    ...theme,
+
+                                    borderRadius: 1,
+
+                                })}
+                            />
+
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography className={classes.heading}>SOIL MOISTURE (SMAP)</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Select className="css-e56m7-control"
+                                defaultValue={ndvidates[2]}
+                                label="Single select"
+                                options={ndvidates}
+                                theme={theme => ({
+                                    ...theme,
+
+                                    borderRadius: 1,
+
+                                })}
+                            />
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel3a-content"
+                            id="panel3a-header"
+                        >
+                            <Typography className={classes.heading}>Evapotranspiration</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Select className="css-e56m7-control"
+                                defaultValue={ndvidates[2]}
+                                label="Single select"
+                                options={ndvidates}
+                                theme={theme => ({
+                                    ...theme,
+
+                                    borderRadius: 1,
+
+                                })}
+                            />
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel3a-content"
+                            id="panel3a-header"
+                        >
+                            <Typography className={classes.heading}>Tree cover</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Select className="css-e56m7-control"
+                                defaultValue={ndvidates[2]}
+                                label="Single select"
+                                options={ndvidates}
+                                theme={theme => ({
+                                    ...theme,
+
+                                    borderRadius: 1,
+
+                                })}
+                            />
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography className={classes.heading}>Tree cover gain</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Select className="css-e56m7-control"
+                                defaultValue={ndvidates[2]}
+                                label="Single select"
+                                options={ndvidates}
+                                theme={theme => ({
+                                    ...theme,
+
+                                    borderRadius: 1,
+
+                                })}
+                            />
+                        </AccordionDetails>
+                    </Accordion>
+
+                </div>
+                {/* Tab2 Data */}
+                <div className={value === 1 ? ' ' : 'hidden'}>
+                    <LayerTree
+                        tasks={tasks}
+                        changeLayer={activateLayer}
+                        category="Two"
+                        state='test'
+                    />
+                    <LayerTree
+                        tasks={tasks}
+                        changeLayer={activateLayer}
+                        category="Natural Resource"
+                    />
+                </div>
+
+            </div>
+            {/* *************Vegetation Tab end*************** */}
+
+
+            {/* *************Weather Tab start*************** */}
             <div className={type === 'Weather' ? 'LayerContainer' : 'hidden'}>
                 <LayerTree
                     tasks={tasks}
@@ -100,7 +308,9 @@ const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1,
                     category="Natural Resource"
                 />
             </div>
+            {/* *************Weather Tab end*************** */}
 
+            {/* *************Water Resources Tab start*************** */}
             <div className={type === 'Water_Resources' ? 'LayerContainer' : 'hidden'}>
                 <LayerTree
                     tasks={tasks}
@@ -114,7 +324,9 @@ const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1,
                     category="Natural Resource"
                 />
             </div>
+            {/* *************Water Resources Tab end*************** */}
 
+            {/* *************Satellite Imagery Tab start*************** */}
             <div className={type === 'Satellite_Imagery' ? 'LayerContainer' : 'hidden'}>
                 <LayerTree
                     tasks={tasks}
@@ -128,8 +340,9 @@ const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1,
                     category="Natural Resource"
                 />
             </div>
+            {/* *************Satellite Imagery Tab end*************** */}
 
-
+            {/* *************Vedas Services Tab start*************** */}
             <div className={type === 'Vedas_Services' ? 'LayerContainer' : 'hidden'}>
                 <LayerTree
                     tasks={tasks}
@@ -143,6 +356,7 @@ const SideBarWrapper = ({ tasks, analyticsLayers, activateLayer, activateLayer1,
                     category="Natural Resource"
                 />
             </div>
+            {/* *************Vedas Services Tab start*************** */}
         </div>
 
 
