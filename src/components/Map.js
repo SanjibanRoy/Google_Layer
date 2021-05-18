cba132b197862c1f1371206ed9617406f0a9b086
nilaynishant <nilaynishant@gmail.com>
2021-05-18 09:14:54 +0530

    Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,34 +1,47 @@
+import { MapContainer, TileLayer, WMSTileLayer, useMap } from "react-leaflet";
+// import { LatLngExpression } from "leaflet";
+import { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { selectDataSet } from "../features/layers/layerslice";
+import { selectLayerDataSet } from "../features/layers/layervisualiseslice";
 
-import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
-import { LatLngExpression } from "leaflet";
-
-const Map = ({places}) => {
-  const style = {
-    width: "100%",
-    bottom: "0px",
-    top: "0px",
-    left: "0px",
-    position: "absolute",
-    margin: 0,
-  };
-  
-  const showPreview = (place) => {
-    // show place's description
+function MyComponent() {
+    const map = useMap()
+    console.log('map center:', map.getCenter())
+    return null
   }
 
+const Map = () => {
+  const mounted = useRef();
+  
+  const state = useSelector(selectDataSet);
+  const layerState = useSelector(selectLayerDataSet);
+  console.log(state);
+  console.log(layerState);
+  // const [mainMap, setmainMap] = useState(0);
+  // const [analyticsLayer, setanalyticsLayer] = useState(null);
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log("Test use effect");
+  });
   return (
-    <div className="map__container" style={style}>
-      <MapContainer
-        center={[51.505, -0.09]} 
-        zoom={13}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
+    <MapContainer center={[26.2006, 92.9376]} zoom={6} zoomControl={false} >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      <WMSTileLayer
+        format="image/png"
+        layers="analytic:ner_landuse_landcover_50k_1st_cycle"
+        url="https://apps.nesdr.gov.in:442/geoserver/wms"
+        transparent="true"
+      />
+      <MyComponent/>
+    </MapContainer>
+    
   );
+  
 };
 
-export default Map
+export default Map;
